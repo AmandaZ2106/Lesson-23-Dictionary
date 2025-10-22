f3f114cf0960b615bbee7b9e283218cb3edb6e30
AmandaZ2106 <romeogcruz@outlook.com>
2025-10-22 19:05:52 +0100

    Country Code

---
+++ country_code.py
@@ -0,0 +1,6 @@
+country_code={'India':200,
+              'UK':498,
+              'Japan':350}
+print("The country code for India:")
+print(country_code.get("India","Not Found"))
+print(country_code.get("Nepal","Not Found"))
